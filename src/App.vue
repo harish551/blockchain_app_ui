590a14feb40476c17548940e88f64ec7c4433fc8
harish <marri.harish@cosmiclabs.co>
2020-11-07 14:27:27 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <layout />
+  </div>
+</template>
+
+<script>
+import initCosmos from '@/mixins/initCosmos'
+
+import Layout from "@/layouts/Layout"
+
+export default {
+  mixins: [initCosmos],
+  components: {
+    Layout,
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: var(--f-primary);
+  color: var(--c-txt-primary);
+  background-color: var(--c-bg-primary);
+}
+
+button:hover {
+  cursor: pointer;
+}
+
+a {
+  color: var(--c-txt-primary);
+}
+a:visited {
+  color: inherit;
+}
+</style>
